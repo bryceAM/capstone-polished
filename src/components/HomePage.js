dee6e6c23bcced6b939cd22883f2fa8792175dd2
bryceAM <bryce.malmberg@yahoo.com>
2024-02-14 00:19:47 -0800

    feat: #12 add FeaturedProducts component to HomePage component

--- src/components/HomePage.js
+++ src/components/HomePage.js
@@ -1,12 +1,12 @@
 import React from 'react';
-// import { FeaturedProducts } from './FeaturedProducts.js';
+import { default as FeaturedProducts } from './FeaturedProducts.js';
 import '../styles/HomePage.css'
 
-function HomePage({ DB }) {
+function HomePage() {
+
     return (
         <div className="header">
-            <img id="welcomeImg" alt="welcome" src="https://i.imgur.com/AleLEV6.png" />
-            {/* <FeaturedProducts DB={DB} /> */}
+            <FeaturedProducts />
         </div>
     );
 };
