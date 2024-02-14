4b14bf70ad2b457ff9ff574ea638392a9acef144
bryceAM <bryce.malmberg@yahoo.com>
2024-02-14 00:10:54 -0800

    feat: #10 add context support to App component

--- src/components/App.js
+++ src/components/App.js
@@ -1,16 +1,21 @@
 import { Routes, Route } from 'react-router-dom'
 import { default as HomePage } from './HomePage.js'
+import Context from '../context.js';
 import '../styles/App.css';
 
 function App() {
-  const DB = ``;
+  const contextValues = {
+    DB: `database string placeholder`
+  }
 
   return (
     <div className="App">
-      <Routes>
-        <Route path="/" element={<HomePage DB={DB} />} />
-        {/* <Route path="/UnderConstruction" element={<UnderConstruction />} /> */}
-      </Routes>
+      <Context.Provider value={contextValues}>
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          {/* <Route path="/UnderConstruction" element={<UnderConstruction />} /> */}
+        </Routes>
+      </Context.Provider>
     </div>
   );
 }
